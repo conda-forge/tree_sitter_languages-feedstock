4ca27c029de63e8470f14dea9a7b5ff8a1309404
Moritz Wilksch <58488209+moritzwilksch@users.noreply.github.com>
2024-02-05 20:53:06 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,5 +3,7 @@
 set -ex
 
 $PYTHON -c "from distutils import ccompiler; ccompiler.show_compilers()"
+$PYTHON -c "from distutils import ccompiler; c = ccompiler.new_compiler(); print(c)"
+
 $PYTHON build.py
 $PYTHON -m pip install . -vv
